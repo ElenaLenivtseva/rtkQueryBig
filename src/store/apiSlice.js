34e3ce1646ca11d244b8d4bf0511bda3a7fffb11
ElenaLenivtseva <108658314+ElenaLenivtseva@users.noreply.github.com>
2024-05-19 16:21:53 +0700

    Создали query чтоб получить все services

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -3,6 +3,9 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl: '/api'}),
     endpoints: (builder) => ({
-
+        getServices: builder.query({
+            query: () => '/services',
     }),
 })
+
+export const {useGetServicesQuery} = api;
